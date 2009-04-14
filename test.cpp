88c04ea06f99a7c7adba0d0912b2ccbca7e9eb8e
Brian Crowell <brian@fluggo.com>
2009-04-13 21:59:28 -0500

    Turned into the VideoWidget class.

--- test.cpp
+++ test.cpp
@@ -126,32 +126,64 @@ static void drawFrame( void *rgb, int width, int height, float pixelAspectRatio 
 #endif
 }
 
-typedef struct {
+class VideoWidget {
+public:
+    VideoWidget( IPresentationClock *clock );
+    ~VideoWidget();
+
+    GtkWidget *getWidget() {
+        return _drawingArea;
+    }
+
+    IPresentationClock *getClock() {
+        return _clock;
+    }
+
+    void stop();
+    void play();
+
+private:
+    GdkGLConfig *_glConfig;
+    GtkWidget *_drawingArea;
     IFrameSource *_source;
     int _timer;
     GMutex *_frameReadMutex;
     GCond *_frameReadCond;
     int _lastDisplayedFrame, _nextToRenderFrame;
-    int readBuffer, writeBuffer, filled;
-    Rational frameRate;
+    int _readBuffer, _writeBuffer, _filled;
+    Rational _frameRate;
     guint _timeoutSourceID;
     IPresentationClock *_clock;
-    int firstFrame, lastFrame;
-    float pixelAspectRatio;
+    int _firstFrame, _lastFrame;
+    float _pixelAspectRatio;
 
     int64_t _presentationTime[4];
-    int64_t nextPresentationTime[4];
+    int64_t _nextPresentationTime[4];
     Array2D<uint8_t[3]> _targets[4];
     float _rate;
-    bool quit;
-} VideoWidgetInfo;
+    bool _quit;
+    GThread *_renderThread;
 
-static gboolean
-videoWidget_expose( GtkWidget *widget, GdkEventExpose *event, gpointer data ) {
-    GdkGLContext *glcontext = gtk_widget_get_gl_context( widget );
-    GdkGLDrawable *gldrawable = gtk_widget_get_gl_drawable( widget );
+    gboolean expose();
+    gboolean playSingleFrame();
+    gpointer playbackThread();
 
-    VideoWidgetInfo *info = (VideoWidgetInfo*) g_object_get_data( G_OBJECT(widget), "__info" );
+    static gpointer playbackThreadCallback( gpointer data ) {
+        return ((VideoWidget*) data)->playbackThread();
+    }
+
+    static gboolean playSingleFrameCallback( gpointer data ) {
+        return ((VideoWidget*) data)->playSingleFrame();
+    }
+
+    static gboolean exposeCallback( GtkWidget *widget, GdkEventExpose *event, gpointer data ) {
+    }
+};
+
+gboolean
+VideoWidget::expose() {
+    GdkGLContext *glcontext = gtk_widget_get_gl_context( _drawingArea );
+    GdkGLDrawable *gldrawable = gtk_widget_get_gl_drawable( _drawingArea );
 
     if( !gdk_gl_drawable_gl_begin( gldrawable, glcontext ) )
         return FALSE;
@@ -163,7 +195,7 @@ videoWidget_expose( GtkWidget *widget, GdkEventExpose *event, gpointer data ) {
         __glewInit = true;
     }
 
-    drawFrame( &info->_targets[info->readBuffer][0][0], 720, 480, info->pixelAspectRatio );
+    drawFrame( &_targets[_readBuffer][0][0], 720, 480, _pixelAspectRatio );
 
     if( gdk_gl_drawable_is_double_buffered( gldrawable ) )
         gdk_gl_drawable_swap_buffers( gldrawable );
@@ -180,53 +212,52 @@ getFrameTime( Rational *frameRate, int frame ) {
     return (int64_t) frame * INT64_C(1000000000) * (int64_t)(frameRate->d) / (int64_t)(frameRate->n);
 }
 
-gpointer PlaybackThread( gpointer data ) {
-    VideoWidgetInfo *info = (VideoWidgetInfo *) data;
-
+gpointer
+VideoWidget::playbackThread() {
     AVFileReader reader( "/home/james/Videos/Okra - 79b,100.avi" );
     Pulldown23RemovalFilter filter( &reader, 0, false );
     Array2D<Rgba> array( 480, 720 );
 
     for( ;; ) {
-        int64_t startTime = info->_clock->getPresentationTime();
-        g_mutex_lock( info->_frameReadMutex );
-        Rational speed = info->_clock->getSpeed();
+        int64_t startTime = _clock->getPresentationTime();
+        g_mutex_lock( _frameReadMutex );
+        Rational speed = _clock->getSpeed();
 
-        while( !info->quit && info->filled > 2 )
-            g_cond_wait( info->_frameReadCond, info->_frameReadMutex );
+        while( !_quit && _filled > 2 )
+            g_cond_wait( _frameReadCond, _frameReadMutex );
 
-        if( info->quit )
+        if( _quit )
             return NULL;
 
-        if( info->filled < 0 )
-            startTime = info->_clock->getPresentationTime();
+        if( _filled < 0 )
+            startTime = _clock->getPresentationTime();
 
-        int nextFrame = info->_nextToRenderFrame;
-        int writeBuffer = (info->writeBuffer = (info->writeBuffer + 1) & 3);
-        g_mutex_unlock( info->_frameReadMutex );
+        int nextFrame = _nextToRenderFrame;
+        int writeBuffer = (_writeBuffer = (_writeBuffer + 1) & 3);
+        g_mutex_unlock( _frameReadMutex );
 
 //        printf( "Start rendering %d into %d...\n", nextFrame, writeBuffer );
 
-        if( nextFrame > info->lastFrame )
-            nextFrame = info->lastFrame;
-        else if( nextFrame < info->firstFrame )
-            nextFrame = info->firstFrame;
+        if( nextFrame > _lastFrame )
+            nextFrame = _lastFrame;
+        else if( nextFrame < _firstFrame )
+            nextFrame = _firstFrame;
 
         filter.GetFrame( nextFrame, array );
 
         // Convert the results to floating-point
         for( int y = 0; y < 480; y++ ) {
             for( int x = 0; x < 720; x++ ) {
-                info->_targets[writeBuffer][479 - y][x][0] = (uint8_t) __gamma45( array[y][x].r );
-                info->_targets[writeBuffer][479 - y][x][1] = (uint8_t) __gamma45( array[y][x].g );
-                info->_targets[writeBuffer][479 - y][x][2] = (uint8_t) __gamma45( array[y][x].b );
+                _targets[writeBuffer][479 - y][x][0] = (uint8_t) __gamma45( array[y][x].r );
+                _targets[writeBuffer][479 - y][x][1] = (uint8_t) __gamma45( array[y][x].g );
+                _targets[writeBuffer][479 - y][x][2] = (uint8_t) __gamma45( array[y][x].b );
             }
         }
 
         //usleep( 100000 );
 
-        info->_presentationTime[writeBuffer] = getFrameTime( &info->frameRate, nextFrame );
-        int64_t endTime = info->_clock->getPresentationTime();
+        _presentationTime[writeBuffer] = getFrameTime( &_frameRate, nextFrame );
+        int64_t endTime = _clock->getPresentationTime();
 
         int64_t lastDuration = endTime - startTime;
 
@@ -234,9 +265,9 @@ gpointer PlaybackThread( gpointer data ) {
         //    ((double) endTime - (double) startTime) / 1000000000.0 );
         //printf( "Presentation time %ld\n", info->_presentationTime[writeBuffer] );
 
-        g_mutex_lock( info->_frameReadMutex );
-        if( info->filled < 0 ) {
-            Rational newSpeed = info->_clock->getSpeed();
+        g_mutex_lock( _frameReadMutex );
+        if( _filled < 0 ) {
+            Rational newSpeed = _clock->getSpeed();
 
             if( speed.n * newSpeed.d != 0 )
                 lastDuration = lastDuration * newSpeed.n * speed.d / (speed.n * newSpeed.d);
@@ -246,30 +277,30 @@ gpointer PlaybackThread( gpointer data ) {
             speed = newSpeed;
 
             if( speed.n > 0 )
-                info->_nextToRenderFrame -= 4;
+                _nextToRenderFrame -= 4;
             else if( speed.n < 0 )
-                info->_nextToRenderFrame += 4;
+                _nextToRenderFrame += 4;
 
-            info->filled = -1;
+            _filled = -1;
 
             // Write where the reader will read next
-            info->writeBuffer = info->readBuffer;
+            _writeBuffer = _readBuffer;
         }
 
-        info->filled++;
+        _filled++;
 
         if( lastDuration < INT64_C(0) )
             lastDuration *= INT64_C(-1);
 
         if( speed.n > 0 ) {
-            while( getFrameTime( &info->frameRate, ++info->_nextToRenderFrame ) < endTime + lastDuration );
+            while( getFrameTime( &_frameRate, ++_nextToRenderFrame ) < endTime + lastDuration );
         }
         else if( speed.n < 0 ) {
-            while( getFrameTime( &info->frameRate, --info->_nextToRenderFrame ) > endTime - lastDuration );
+            while( getFrameTime( &_frameRate, --_nextToRenderFrame ) > endTime - lastDuration );
         }
 
-        info->nextPresentationTime[writeBuffer] = getFrameTime( &info->frameRate, info->_nextToRenderFrame );
-        g_mutex_unlock( info->_frameReadMutex );
+        _nextPresentationTime[writeBuffer] = getFrameTime( &_frameRate, _nextToRenderFrame );
+        g_mutex_unlock( _frameReadMutex );
 
 /*            std::stringstream filename;
         filename << "rgba" << i++ << ".exr";
@@ -287,61 +318,142 @@ gpointer PlaybackThread( gpointer data ) {
 }
 
 gboolean
-playSingleFrame( gpointer data ) {
-    GtkWidget *widget = (GtkWidget*) data;
-    VideoWidgetInfo *info = (VideoWidgetInfo*) g_object_get_data( G_OBJECT(widget), "__info" );
-
-    if( info->filled > 0 ) {
-        g_mutex_lock( info->_frameReadMutex );
-        int filled = info->filled;
-        info->readBuffer = (info->readBuffer + 1) & 3;
-        int64_t nextPresentationTime = info->nextPresentationTime[info->readBuffer];
-        g_mutex_unlock( info->_frameReadMutex );
+VideoWidget::playSingleFrame() {
+    if( _filled > 0 ) {
+        g_mutex_lock( _frameReadMutex );
+        int filled = _filled;
+        _readBuffer = (_readBuffer + 1) & 3;
+        int64_t nextPresentationTime = _nextPresentationTime[_readBuffer];
+        g_mutex_unlock( _frameReadMutex );
 
         if( filled != 0 ) {
-            gdk_window_invalidate_rect( widget->window, &widget->allocation, FALSE );
-            gdk_window_process_updates( widget->window, FALSE );
+            gdk_window_invalidate_rect( _drawingArea->window, &_drawingArea->allocation, FALSE );
+            gdk_window_process_updates( _drawingArea->window, FALSE );
 
             //printf( "Painted %ld from %d...\n", info->_presentationTime[info->readBuffer], info->readBuffer );
 
-            g_mutex_lock( info->_frameReadMutex );
+            g_mutex_lock( _frameReadMutex );
 
-            info->filled--;
+            _filled--;
 
-            g_cond_signal( info->_frameReadCond );
-            g_mutex_unlock( info->_frameReadMutex );
+            g_cond_signal( _frameReadCond );
+            g_mutex_unlock( _frameReadMutex );
 
-            Rational speed = info->_clock->getSpeed();
+            Rational speed = _clock->getSpeed();
 
-            int timeout = (nextPresentationTime - info->_clock->getPresentationTime()) * speed.d / (speed.n * 1000000);
+            int timeout = (nextPresentationTime - _clock->getPresentationTime()) * speed.d / (speed.n * 1000000);
             //printf( "timeout %d\n", timeout );
 
             if( timeout < 0 )
                 timeout = 0;
 
-            info->_timeoutSourceID = g_timeout_add( timeout, playSingleFrame, data );
+            _timeoutSourceID = g_timeout_add( timeout, playSingleFrameCallback, this );
             return FALSE;
         }
     }
 
-    Rational speed = info->_clock->getSpeed();
+    Rational speed = _clock->getSpeed();
 
-    info->_timeoutSourceID = g_timeout_add(
-        (1000 * info->frameRate.d * speed.d) / (info->frameRate.n * abs(speed.n)),
-        playSingleFrame, data );
+    _timeoutSourceID = g_timeout_add(
+        (1000 * _frameRate.d * speed.d) / (_frameRate.n * abs(speed.n)),
+        playSingleFrameCallback, this );
     return FALSE;
+}
+
+VideoWidget::VideoWidget( IPresentationClock *clock ) {
+    _glConfig = gdk_gl_config_new_by_mode ( (GdkGLConfigMode) (GDK_GL_MODE_RGB    |
+                                        GDK_GL_MODE_DEPTH  |
+                                        GDK_GL_MODE_DOUBLE));
+    if( _glConfig == NULL )    {
+        g_print( "*** Cannot find the double-buffered visual.\n" );
+        g_print( "*** Trying single-buffered visual.\n" );
+
+        /* Try single-buffered visual */
+        _glConfig = gdk_gl_config_new_by_mode ((GdkGLConfigMode) (GDK_GL_MODE_RGB   |
+                                        GDK_GL_MODE_DEPTH));
+        if( _glConfig == NULL ) {
+            g_print( "*** No appropriate OpenGL-capable visual found.\n" );
+            exit( 1 );
+        }
+    }
+
+    _drawingArea = gtk_drawing_area_new();
+    gtk_widget_set_size_request( _drawingArea, 720, 480 );
+
+    gtk_widget_set_gl_capability( _drawingArea,
+                                _glConfig,
+                                NULL,
+                                TRUE,
+                                GDK_GL_RGBA_TYPE );
+
+    _clock = clock;
+    _frameReadMutex = g_mutex_new();
+    _frameReadCond = g_cond_new();
+    _nextToRenderFrame = 5000;
+    _frameRate = Rational( 24000, 1001 );
+    _filled = -1;
+    _readBuffer = 3;
+    _writeBuffer = 3;
+    _firstFrame = 0;
+    _lastFrame = 6000;
+    _pixelAspectRatio = 40.0f / 33.0f;
+    _quit = false;
+    _targets[0].resizeErase( 480, 720 );
+    _targets[1].resizeErase( 480, 720 );
+    _targets[2].resizeErase( 480, 720 );
+    _targets[3].resizeErase( 480, 720 );
+
+    g_object_set_data( G_OBJECT(_drawingArea), "__info", this );
+    g_signal_connect( G_OBJECT(_drawingArea), "expose_event", G_CALLBACK(exposeCallback), NULL );
+
+    g_timeout_add( 0, playSingleFrameCallback, this );
+    _renderThread = g_thread_create( playbackThreadCallback, this, TRUE, NULL );
+}
+
+VideoWidget::~VideoWidget() {
+    // Stop the render thread
+    g_mutex_lock( _frameReadMutex );
+    _quit = true;
+    g_cond_signal( _frameReadCond );
+    g_mutex_unlock( _frameReadMutex );
+
+    g_thread_join( _renderThread );
+}
+
+void
+VideoWidget::stop() {
+    if( _timeoutSourceID != 0 ) {
+        g_source_remove( _timeoutSourceID );
+        _timeoutSourceID = 0;
+    }
+
+    // Just stop the production thread
+    g_mutex_lock( _frameReadMutex );
+    _filled = 3;
+    g_mutex_unlock( _frameReadMutex );
+}
+
+void
+VideoWidget::play() {
+    if( _timeoutSourceID != 0 ) {
+        g_source_remove( _timeoutSourceID );
+        _timeoutSourceID = 0;
+    }
+
+    // Fire up the production and playback threads from scratch
+    g_mutex_lock( _frameReadMutex );
+    _filled = -2;
+    g_cond_signal( _frameReadCond );
+    g_mutex_unlock( _frameReadMutex );
+
+    playSingleFrame();
 }
 
 gboolean
 keyPressHandler( GtkWidget *widget, GdkEventKey *event, gpointer userData ) {
-    VideoWidgetInfo *info = (VideoWidgetInfo*) g_object_get_data( G_OBJECT((GtkWidget*) userData), "__info" );
+    VideoWidget *video = (VideoWidget*) g_object_get_data( G_OBJECT((GtkWidget*) userData), "__info" );
 
-    if( info->_timeoutSourceID != 0 ) {
-        g_source_remove( info->_timeoutSourceID );
-        info->_timeoutSourceID = 0;
-    }
-
-    Rational speed = info->_clock->getSpeed();
+    Rational speed = video->getClock()->getSpeed();
 
     switch( event->keyval ) {
         case GDK_l:
@@ -363,23 +475,12 @@ keyPressHandler( GtkWidget *widget, GdkEventKey *event, gpointer userData ) {
             break;
     }
 
-    ((SystemPresentationClock*) info->_clock)->play( speed );
+    ((SystemPresentationClock*) video->getClock())->play( speed );
 
-    if( speed.n == 0 ) {
-        // Just stop the production thread
-        g_mutex_lock( info->_frameReadMutex );
-        info->filled = 3;
-        g_mutex_unlock( info->_frameReadMutex );
-    }
-    else {
-        // Fire up the production and playback threads from scratch
-        g_mutex_lock( info->_frameReadMutex );
-        info->filled = -2;
-        g_cond_signal( info->_frameReadCond );
-        g_mutex_unlock( info->_frameReadMutex );
-
-        playSingleFrame( (GtkWidget*) userData );
-    }
+    if( speed.n == 0 )
+        video->stop();
+    else
+        video->play();
 
     return TRUE;
 }
@@ -424,26 +525,6 @@ main( int argc, char *argv[] ) {
     if( !g_thread_supported() )
         g_thread_init( NULL );
 
-    GdkGLConfig *glconfig;
-
-    glconfig = gdk_gl_config_new_by_mode ( (GdkGLConfigMode) (GDK_GL_MODE_RGB    |
-                                        GDK_GL_MODE_DEPTH  |
-                                        GDK_GL_MODE_DOUBLE));
-    if (glconfig == NULL)
-    {
-        g_print ("*** Cannot find the double-buffered visual.\n");
-        g_print ("*** Trying single-buffered visual.\n");
-
-        /* Try single-buffered visual */
-        glconfig = gdk_gl_config_new_by_mode ((GdkGLConfigMode) (GDK_GL_MODE_RGB   |
-                                        GDK_GL_MODE_DEPTH));
-        if (glconfig == NULL)
-        {
-            g_print ("*** No appropriate OpenGL-capable visual found.\n");
-            exit (1);
-        }
-    }
-
 /*    AVFileReader reader( "/home/james/Desktop/Demo2.avi" );
     Array2D<Rgba> array( 480, 720 );
     int i = 0;
@@ -464,40 +545,12 @@ main( int argc, char *argv[] ) {
     GtkWidget *window = gtk_window_new( GTK_WINDOW_TOPLEVEL );
     gtk_window_set_title( GTK_WINDOW(window), "boogidy boogidy" );
 
-    GtkWidget *drawingArea = gtk_drawing_area_new();
-    gtk_widget_set_size_request( drawingArea, 720, 480 );
-
-    gtk_widget_set_gl_capability( drawingArea,
-                                glconfig,
-                                NULL,
-                                TRUE,
-                                GDK_GL_RGBA_TYPE );
-
-    int h = 480, w = 720;
-
     SystemPresentationClock clock;
     clock.set( Rational( 1, 1 ), 5000LL * 1000000000LL * 1001LL / 24000LL );
 
-    VideoWidgetInfo info;
-    info._clock = &clock;
-    info._frameReadMutex = g_mutex_new();
-    info._frameReadCond = g_cond_new();
-    info._nextToRenderFrame = 5000;
-    info.frameRate = Rational( 24000, 1001 );
-    info.filled = -1;
-    info.readBuffer = 3;
-    info.writeBuffer = 3;
-    info.firstFrame = 0;
-    info.lastFrame = 6000;
-    info.pixelAspectRatio = 40.0f / 33.0f;
-    info.quit = false;
-    info._targets[0].resizeErase( h, w );
-    info._targets[1].resizeErase( h, w );
-    info._targets[2].resizeErase( h, w );
-    info._targets[3].resizeErase( h, w );
+    VideoWidget widget( &clock );
+    GtkWidget *drawingArea = widget.getWidget();
 
-    g_object_set_data( G_OBJECT(drawingArea), "__info", &info );
-    g_signal_connect( G_OBJECT(drawingArea), "expose_event", G_CALLBACK(videoWidget_expose), NULL );
     g_signal_connect( G_OBJECT(window), "key-press-event", G_CALLBACK(keyPressHandler), drawingArea );
     g_signal_connect( G_OBJECT(window), "delete_event", G_CALLBACK(gtk_main_quit), NULL );
 
@@ -506,18 +559,7 @@ main( int argc, char *argv[] ) {
 
     gtk_widget_show( window );
 
-    g_timeout_add( 0, playSingleFrame, drawingArea );
-    GThread *thread = g_thread_create( PlaybackThread, &info, TRUE, NULL );
-
     gtk_main();
-
-    // Stop the render thread
-    g_mutex_lock( info._frameReadMutex );
-    info.quit = true;
-    g_cond_signal( info._frameReadCond );
-    g_mutex_unlock( info._frameReadMutex );
-
-    g_thread_join( thread );
 }
 
 
