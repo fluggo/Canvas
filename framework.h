5bd4643f4b09263c4211fb433053a724bfcdc007
Brian Crowell <brian@fluggo.com>
2009-04-19 18:32:13 -0500

    Removed the remaining classes. It's all Python now.

--- framework.h
+++ framework.h
@@ -42,21 +42,3 @@ typedef struct {
 
 NOEXPORT int takeVideoSource( PyObject *source, VideoSourceHolder *holder );
 
-class IFrameSource {
-public:
-    virtual void GetFrame( int64_t frame, Imf::Array2D<Imf::Rgba> &array ) = 0;
-};
-
-class Pulldown23RemovalFilter : public IFrameSource {
-public:
-    Pulldown23RemovalFilter( IFrameSource *source, int offset, bool oddFirst );
-
-    virtual void GetFrame( int64_t frame, Imf::Array2D<Imf::Rgba> &array );
-
-private:
-    IFrameSource *_source;
-    int _offset;
-    bool _oddFirst;
-};
-
-
