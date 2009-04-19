1b69fa38a2abf712448a5ef388d80db2f10bfd2f
Brian Crowell <brian@fluggo.com>
2009-04-19 13:37:16 -0500

    Removed old AVFileSource class definition.

--- framework.h
+++ framework.h
@@ -45,25 +45,6 @@ public:
     virtual void GetFrame( int64_t frame, Imf::Array2D<Imf::Rgba> &array ) = 0;
 };
 
-class AVFileReader : public IFrameSource {
-public:
-    AVFileReader( const char *filename );
-    ~AVFileReader();
-
-    virtual void GetFrame( int64_t frame, Imf::Array2D<Imf::Rgba> &array );
-
-private:
-    AVFormatContext *_context;
-    AVCodecContext *_codecContext;
-    AVCodec *_codec;
-    int _firstVideoStream;
-    AVFrame *_inputFrame, *_rgbFrame;
-    uint8_t *_rgbBuffer;
-    float _colorMatrix[3][3];
-    struct SwsContext *_scaler;
-
-};
-
 class Pulldown23RemovalFilter : public IFrameSource {
 public:
     Pulldown23RemovalFilter( IFrameSource *source, int offset, bool oddFirst );
