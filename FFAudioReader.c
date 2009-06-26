9650523556d471232ef0bd610c8654609ffec812
Brian Crowell <brian@fluggo.com>
2009-06-26 17:05:59 -0500

    Added support for other sample types, as well as fixed interpretation of timestamps (it works like video in that regard).

--- FFAudioReader.c
+++ FFAudioReader.c
@@ -12,7 +12,7 @@ typedef struct {
     int firstAudioStream;
     AVFrame *inputFrame;
     bool allKeyframes;
-    int16_t *scratchyRadioBuffer;
+    void *audioBuffer;
     int lastPacketStart, lastPacketDuration;
 } py_obj_FFAudioReader;
 
@@ -67,9 +67,9 @@ FFAudioReader_init( py_obj_FFAudioReader *self, PyObject *args, PyObject *kwds )
         return -1;
     }
 
-    self->scratchyRadioBuffer = malloc( sizeof(int16_t) * AVCODEC_MAX_AUDIO_FRAME_SIZE );
+    self->audioBuffer = PyMem_Malloc( AVCODEC_MAX_AUDIO_FRAME_SIZE );
 
-    if( !self->scratchyRadioBuffer ) {
+    if( !self->audioBuffer ) {
         PyErr_NoMemory();
         return -1;
     }
@@ -104,10 +104,8 @@ FFAudioReader_init( py_obj_FFAudioReader *self, PyObject *args, PyObject *kwds )
 
 static void
 FFAudioReader_dealloc( py_obj_FFAudioReader *self ) {
-    if( self->scratchyRadioBuffer != NULL ) {
-        free( self->scratchyRadioBuffer );
-        self->scratchyRadioBuffer = NULL;
-    }
+    PyMem_Free( self->audioBuffer );
+    self->audioBuffer = NULL;
 
     if( self->codecContext != NULL ) {
         avcodec_close( self->codecContext );
@@ -122,6 +120,98 @@ FFAudioReader_dealloc( py_obj_FFAudioReader *self ) {
     self->ob_type->tp_free( (PyObject*) self );
 }
 
+static void convert_uint8( float *out, int outChannels, uint8_t *in, int inChannels, int duration ) {
+    int channelCount = min(outChannels, inChannels);
+
+    for( int sample = 0; sample < duration; sample++ ) {
+        for( int channel = 0; channel < channelCount; channel++ ) {
+            out[outChannels * sample + channel] = (float) in[inChannels * sample + channel] * (1.0f / (float) INT8_MAX) - 1.0f;
+        }
+    }
+}
+
+static void convert_int16( float *out, int outChannels, int16_t *in, int inChannels, int duration ) {
+    int channelCount = min(outChannels, inChannels);
+
+    for( int sample = 0; sample < duration; sample++ ) {
+        for( int channel = 0; channel < channelCount; channel++ ) {
+            out[outChannels * sample + channel] = (float) in[inChannels * sample + channel] * (1.0f / (float) INT16_MAX);
+        }
+    }
+}
+
+static void convert_int32( float *out, int outChannels, int32_t *in, int inChannels, int duration ) {
+    int channelCount = min(outChannels, inChannels);
+
+    for( int sample = 0; sample < duration; sample++ ) {
+        for( int channel = 0; channel < channelCount; channel++ ) {
+            out[outChannels * sample + channel] = (float) in[inChannels * sample + channel] * (1.0f / (float) INT32_MAX);
+        }
+    }
+}
+
+static void convert_float( float *out, int outChannels, float *in, int inChannels, int duration ) {
+    int channelCount = min(outChannels, inChannels);
+
+    for( int sample = 0; sample < duration; sample++ ) {
+        for( int channel = 0; channel < channelCount; channel++ ) {
+            out[outChannels * sample + channel] = in[inChannels * sample + channel];
+        }
+    }
+}
+
+static void convert_double( float *out, int outChannels, double *in, int inChannels, int duration ) {
+    int channelCount = min(outChannels, inChannels);
+
+    for( int sample = 0; sample < duration; sample++ ) {
+        for( int channel = 0; channel < channelCount; channel++ ) {
+            out[outChannels * sample + channel] = (float) in[inChannels * sample + channel];
+        }
+    }
+}
+
+static void convert_samples( float *out, int outChannels, void *in, int inChannels, int offset, enum SampleFormat sample_fmt, int duration ) {
+    switch( sample_fmt ) {
+        case SAMPLE_FMT_U8:
+            convert_uint8( out, outChannels,
+                ((uint8_t *) in) + offset * inChannels, inChannels, duration );
+            return;
+
+        case SAMPLE_FMT_S16:
+            convert_int16( out, outChannels,
+                ((int16_t *) in) + offset * inChannels, inChannels, duration );
+            return;
+
+        case SAMPLE_FMT_S32:
+            convert_int32( out, outChannels,
+                ((int32_t *) in) + offset * inChannels, inChannels, duration );
+            return;
+
+        case SAMPLE_FMT_FLT:
+            convert_float( out, outChannels,
+                ((float *) in) + offset * inChannels, inChannels, duration );
+            return;
+
+        case SAMPLE_FMT_DBL:
+            convert_double( out, outChannels,
+                ((double *) in) + offset * inChannels, inChannels, duration );
+            return;
+
+        default:
+            printf( "Unknown sample type.\n" );
+            return;
+    }
+}
+
+static int getSampleCount( int byteCount, enum SampleFormat sample_fmt, int channels ) {
+    static int formatSize[] = { 1, 2, 4, 4, 8 };
+
+    if( sample_fmt < 0 || sample_fmt > 4 )
+        sample_fmt = 1;
+
+    return byteCount / (formatSize[sample_fmt] * channels);
+}
+
 static void
 FFAudioReader_getFrame( py_obj_FFAudioReader *self, AudioFrame *frame ) {
 /*    if( frameIndex < 0 || frameIndex > self->context->streams[self->firstAudioStream]->duration ) {
@@ -133,7 +223,12 @@ FFAudioReader_getFrame( py_obj_FFAudioReader *self, AudioFrame *frame ) {
 
     //printf( "Requested %ld\n", frameIndex );
 
-    int64_t timestamp = frame->fullMinSample;
+    AVRational *timeBase = &self->context->streams[self->firstAudioStream]->time_base;
+    int sampleRate = self->codecContext->sample_rate;
+    //printf( "timeBase: %d/%d, frameRate: %d/%d\n", timeBase->num, timeBase->den, sampleRate, 1 );
+    int64_t frameDuration = (timeBase->den) / (timeBase->num * sampleRate);
+    int64_t timestamp = frame->fullMinSample * (timeBase->den) / (timeBase->num * sampleRate) + frameDuration / 2;
+
 
 //    if( (uint64_t) self->context->start_time != AV_NOPTS_VALUE )
 //        timestamp += self->context->start_time;
@@ -161,22 +256,27 @@ FFAudioReader_getFrame( py_obj_FFAudioReader *self, AudioFrame *frame ) {
 //    }
 
     bool first = true;
-    int channelCount = min(frame->channelCount, self->codecContext->channels);
 
     if( self->lastPacketStart != -1 && self->lastPacketStart <= frame->fullMaxSample &&
         (self->lastPacketStart + self->lastPacketDuration) >= frame->fullMinSample ) {
 
         // Decode into the current frame
-        for( int sample = max(self->lastPacketStart, frame->fullMinSample); sample < min(self->lastPacketStart + self->lastPacketDuration, frame->fullMaxSample + 1); sample++ ) {
-            for( int channel = 0; channel < channelCount; channel++ ) {
-                frame->frameData[frame->channelCount * (sample - frame->fullMinSample) + channel] =
-                    (float) self->scratchyRadioBuffer[(sample - self->lastPacketStart) * self->codecContext->channels + channel] * (1.0f / 32768.0f);
-            }
-        }
+        int startSample = max(self->lastPacketStart, frame->fullMinSample);
+        int duration = min(self->lastPacketStart + self->lastPacketDuration, frame->fullMaxSample + 1) - startSample;
+        float *out = frame->frameData + (startSample - frame->fullMinSample) * frame->channelCount;
 
-        frame->currentMinSample = self->lastPacketStart;
-        frame->currentMaxSample = self->lastPacketDuration;
+        convert_samples( out, frame->channelCount, self->audioBuffer, self->codecContext->channels, (startSample - self->lastPacketStart),
+            self->codecContext->sample_fmt, duration );
+
+        frame->currentMinSample = startSample;
+        frame->currentMaxSample = startSample + duration - 1;
         first = false;
+
+        // We could be done...
+        if( frame->currentMinSample == frame->fullMinSample && frame->currentMaxSample == frame->fullMaxSample ) {
+            //printf( "Going home early: (%d, %d)\n", startSample, duration );
+            return;
+        }
     }
 
     for( ;; ) {
@@ -201,44 +301,62 @@ FFAudioReader_getFrame( py_obj_FFAudioReader *self, AudioFrame *frame ) {
             continue;
         }
 
-        int dataSize = sizeof(int16_t) * AVCODEC_MAX_AUDIO_FRAME_SIZE;
+        int bufferSize = AVCODEC_MAX_AUDIO_FRAME_SIZE;
+        uint8_t *data = packet.data;
+        int dataSize = packet.size;
+        uint8_t *audioBuffer = self->audioBuffer;
 
-        //printf( "Decoding video\n" );
-        // BJC: Here. Enjoy this marvelous cosmic joke: FFMPEG decodes all kinds of formats
-        // with exquisite attention to detail. Then it hammers them all into 16-bit shorts.
-        if( avcodec_decode_audio2( self->codecContext, self->scratchyRadioBuffer, &dataSize,
-            packet.data, packet.size ) < 0 ) {
-            printf( "Could not decode the audio.\n" );
-            frame->currentMaxSample = -1;
-            frame->currentMinSample = 0;
-            return;
-        }
+        int packetStart = (packet.dts * timeBase->num * sampleRate) / (timeBase->den);
+        int packetDuration = 0;
 
-        if( dataSize <= 0 ) {
-            //printf( "Didn't get a picture\n" );
-            av_free_packet( &packet );
-            continue;
-        }
+        while( dataSize > 0 ) {
+            int decoded;
 
-        self->lastPacketStart = packet.dts;
-        self->lastPacketDuration = packet.duration;
-
-        //printf( "We'll take that\n" );
-        for( int sample = max(packet.dts, frame->fullMinSample); sample < min(packet.dts + packet.duration, frame->fullMaxSample + 1); sample++ ) {
-            for( int channel = 0; channel < channelCount; channel++ ) {
-                frame->frameData[frame->channelCount * (sample - frame->fullMinSample) + channel] =
-                    (float) self->scratchyRadioBuffer[(sample - packet.dts) * self->codecContext->channels + channel] * (1.0f / 32768.0f);
+            //printf( "Decoding audio (size left: %d)\n", dataSize );
+            if( (decoded = avcodec_decode_audio2( self->codecContext, audioBuffer, &bufferSize,
+                data, dataSize )) < 0 ) {
+                printf( "Could not decode the audio.\n" );
+                frame->currentMaxSample = -1;
+                frame->currentMinSample = 0;
+                av_free_packet( &packet );
+                return;
             }
+
+            if( bufferSize <= 0 ) {
+                //printf( "Didn't get a sound\n" );
+                av_free_packet( &packet );
+                continue;
+            }
+
+            //printf( "Decoded %d bytes, got %d bytes\n", decoded, bufferSize );
+
+            packetDuration += getSampleCount( bufferSize, self->codecContext->sample_fmt, self->codecContext->channels );
+
+            data += decoded;
+            dataSize -= decoded;
+            audioBuffer += bufferSize;
+            bufferSize = AVCODEC_MAX_AUDIO_FRAME_SIZE;        // A lie, but a safe one, I think
         }
+
+        self->lastPacketStart = packetStart;
+        self->lastPacketDuration = packetDuration;
+
+        //printf( "We'll take that (%d, %d)\n", packetStart, packetDuration );
+        int startSample = max(packetStart, frame->fullMinSample);
+        int duration = min(packetStart + packetDuration, frame->fullMaxSample + 1) - startSample;
+        float *out = frame->frameData + (startSample - frame->fullMinSample) * frame->channelCount;
+
+        convert_samples( out, frame->channelCount, self->audioBuffer, self->codecContext->channels, (startSample - packetStart),
+            self->codecContext->sample_fmt, duration );
 
         if( first ) {
-            frame->currentMinSample = packet.dts;
-            frame->currentMaxSample = packet.dts + packet.duration - 1;
+            frame->currentMinSample = packetStart;
+            frame->currentMaxSample = packetStart + packetDuration - 1;
             first = false;
         }
         else {
-            frame->currentMinSample = min(frame->currentMinSample, packet.dts);
-            frame->currentMaxSample = max(frame->currentMaxSample, packet.dts + packet.duration - 1);
+            frame->currentMinSample = min(frame->currentMinSample, packetStart);
+            frame->currentMaxSample = max(frame->currentMaxSample, packetStart + packetDuration - 1);
         }
 
         frame->currentMinSample = max(frame->currentMinSample, frame->fullMinSample);
@@ -246,7 +364,7 @@ FFAudioReader_getFrame( py_obj_FFAudioReader *self, AudioFrame *frame ) {
 
         av_free_packet( &packet );
 
-        if( packet.dts + packet.duration >= frame->fullMaxSample ) {
+        if( packetStart + packetDuration >= frame->fullMaxSample ) {
 //            printf( "Enough: (%d, %d) vs (%d, %d)\n", frame->currentMinSample, frame->currentMaxSample, frame->fullMinSample, frame->fullMaxSample );
             return;
         }
