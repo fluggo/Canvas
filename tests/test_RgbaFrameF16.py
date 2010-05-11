27567c9299966f9ae81b905e087a1e7bcd12b348
Brian Crowell <brian@fluggo.com>
2010-05-11 17:42:49 -0500

    Tests for RgbaFrameF16.

---
+++ tests/test_RgbaFrameF16.py
@@ -0,0 +1,23 @@
+import unittest
+from fluggo.media import process
+
+class test_RgbaFrameF16(unittest.TestCase):
+    def test_solid(self):
+        color = (1.0, 0.5, 0.333333, 0.2)
+        solid = process.SolidColorVideoSource(color, (0, 0, 2, 2))
+        frame = process.get_frame_f16(solid, 0, (0, 0, 3, 3))
+
+        self.assertEqual(frame.current_data_window, (0, 0, 2, 2))
+        self.assertEqual(frame.full_data_window, (0, 0, 3, 3))
+
+        for x, y in zip(frame.pixel(0, 0), color):
+            self.assertAlmostEqual(x, y, 3)
+
+        frame2 = process.get_frame_f16(frame, 0, (-1, -1, 1, 1))
+
+        self.assertEqual(frame2.current_data_window, (0, 0, 1, 1))
+        self.assertEqual(frame2.full_data_window, (-1, -1, 1, 1))
+
+        for x, y in zip(frame.pixel(0, 0), color):
+            self.assertAlmostEqual(x, y, 3)
+
