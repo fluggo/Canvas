2ccbb2e9b299314da628e813e5f4293456fd1449
Brian Crowell <brian@fluggo.com>
2010-05-23 21:52:32 -0500

    Bring the qttest up to date with its first menu commands and very simple transport controls.
    This version, for whatever reason, segfaults on exit on my machine. I haven't
    tested it against a different Qt4/PyQt4 build, so I can't be sure, but gdb
    tells me the error happens in their code, and for no obvious reason (this is
    on Ubuntu Lucid).

--- qttest.py
+++ qttest.py
@@ -14,60 +14,103 @@ class VideoClip(process.VideoPassThroughFilter):
         self.pixel_aspect_ratio = pixel_aspect_ratio
         self.length = length
 
-videro = process.FFVideoSource('/home/james/Videos/Soft Boiled/Sources/softboiled01;17;55;12.avi')
-pulldown = process.Pulldown23RemovalFilter(videro, 0);
-
-clip = VideoClip(pulldown, 300, fractions.Fraction(640, 704), box2i(0, -1, 719, 478))
-
-#videro = process.FFVideoSource('/home/james/Videos/tape-1999-uil-football-fame.dv')
-#clip = VideoClip(videro, 300, fractions.Fraction(640, 704), box2i(0, -1, 719, 478))
-
-red = process.SolidColorVideoSource(rgba(1.0, 0.0, 0.0, 0.25), box2i(20, 20, 318, 277))
-green = process.SolidColorVideoSource(rgba(0.0, 1.0, 0.0, 0.75), box2i(200, 200, 518, 477))
-
-frame = videro.get_frame_f32(0, box2i(200, 100, 519, 278))
-
-workspace = process.Workspace()
-workspace_item = workspace.add(source=clip, x=0, width=100, z=0, offset=0)
-workspace.add(source=red, x=50, width=100, z=1)
-workspace.add(source=green, x=75, width=100, z=2)
-workspace.add(source=frame, x=125, width=100, z=0, offset=500)
-
-#videro = process.VideoMixFilter(src_a=process.SolidColorVideoSource((1.0, 0.0, 0.0, 0.25), (1, 0, 718, 477)), src_b=process.SolidColorVideoSource((0.0, 1.0, 0.0, 0.75), (2, 1, 717, 476)), mix_b=process.LinearFrameFunc(a=1/300.0, b=0))
-audio = process.FFAudioSource('/home/james/Videos/Soft Boiled/Sources/BWFF/1B_1.wav')
-player = process.AlsaPlayer(48000, source=audio)
-clock = player
-
-NS_PER_SEC = fractions.Fraction(1000000000L, 1)
-
 class MainWindow(QMainWindow):
     def __init__(self):
         QMainWindow.__init__(self)
 
-        self.view = canvas.View(clock)
+        # BJC: This is the initial test code; as soon as load/save support is in, this goes away
+        videro = process.FFVideoSource('/home/james/Videos/Soft Boiled/Sources/softboiled01;17;55;12.avi')
+        pulldown = process.Pulldown23RemovalFilter(videro, 0);
+
+        clip = VideoClip(pulldown, 300, fractions.Fraction(640, 704), box2i(0, -1, 719, 478))
+
+        workspace = process.Workspace()
+        workspace_item = workspace.add(source=clip, x=0, width=100, z=0, offset=0)
+
+        # Set up canvas
+        self.clock = process.SystemPresentationClock()
+
+        self.view = canvas.View(self.clock)
         #self.view.setViewport(QGLWidget())
         self.view.setBackgroundBrush(QBrush(QColor.fromRgbF(0.5, 0.5, 0.5)))
-        self.setCentralWidget(self.view)
 
         item = canvas.VideoItem(workspace_item, 'Clip')
         self.view.scene().addItem(item)
         item.setSelected(True)
 
         format = QGLFormat()
-        self.video_widget = qt.VideoWidget(format)
-        self.video_dock = QDockWidget('Preview')
+        self.video_dock = QDockWidget('Video Preview', self)
+        self.video_widget = qt.VideoWidget(format, self.video_dock)
         self.video_dock.setWidget(self.video_widget)
 
         self.video_widget.setDisplayWindow(box2i(0, -1, 719, 478))
 
         self.video_widget.setRenderingIntent(1.5)
         self.video_widget.setPixelAspectRatio(640.0/704.0)
-        self.video_widget.setPresentationClock(clock)
+        self.video_widget.setPresentationClock(self.clock)
         self.video_widget.setVideoSource(workspace)
 
-        clock.seek(0)
+        self.clock.seek(0)
 
         self.addDockWidget(Qt.BottomDockWidgetArea, self.video_dock)
+
+        # Set up UI
+        self.create_actions()
+        self.create_menus()
+
+        center_widget = QWidget(self)
+        layout = QVBoxLayout(center_widget)
+        layout.addWidget(self.view)
+
+        transport_toolbar = QToolBar(self)
+        transport_toolbar.addAction(self.transport_play_action)
+        transport_toolbar.addAction(self.transport_pause_action)
+        layout.addWidget(transport_toolbar)
+        center_widget.setLayout(layout)
+
+        self.setCentralWidget(center_widget)
+
+    def create_actions(self):
+        self.add_clip_action = QAction('&Add Clip...', self,
+            statusTip='Add a new clip to the canvas', triggered=self.add_clip)
+        self.quit_action = QAction('&Quit', self, shortcut=QKeySequence.Quit,
+            statusTip='Quit the application', triggered=self.close, menuRole=QAction.QuitRole)
+
+        self.view_video_preview = self.video_dock.toggleViewAction()
+        self.view_video_preview.setText('Video &Preview')
+
+        self.transport_group = QActionGroup(self)
+        self.transport_play_action = QAction('Play', self.transport_group,
+            statusTip='Play the current timeline', triggered=self.transport_play,
+            icon=self.style().standardIcon(QStyle.SP_MediaPlay))
+        self.transport_pause_action = QAction('Pause', self.transport_group,
+            statusTip='Pause the current timeline', triggered=self.transport_pause,
+            icon=self.style().standardIcon(QStyle.SP_MediaPause))
+
+    def create_menus(self):
+        self.file_menu = self.menuBar().addMenu('&File')
+        self.file_menu.addAction(self.add_clip_action)
+        self.file_menu.addSeparator()
+        self.file_menu.addAction(self.quit_action)
+
+        self.view_menu = self.menuBar().addMenu('&View')
+        self.view_menu.addAction(self.view_video_preview)
+
+    def add_clip(self):
+        file_name = str(QFileDialog.getOpenFileName(self, 'Add Clip'))
+
+        videro = process.FFVideoSource(file_name)
+
+        clip = VideoClip(videro, 300, fractions.Fraction(640, 704), box2i(0, -1, 719, 478))
+        workspace_item = workspace.add(source=clip, x=0, width=100, z=0, offset=0)
+        item = canvas.VideoItem(workspace_item, 'Clip')
+        self.view.scene().addItem(item)
+
+    def transport_play(self):
+        self.clock.play(1)
+
+    def transport_pause(self):
+        self.clock.stop()
 
 app = QApplication(sys.argv)
 
