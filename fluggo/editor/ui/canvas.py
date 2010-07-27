96266a220df9a4281fe51396af99ae205d2080aa
Brian Crowell <brian@fluggo.com>
2010-07-27 14:41:49 -0500

    Define markers that are dependent on the view and not the scene.

--- fluggo/editor/ui/canvas.py
+++ fluggo/editor/ui/canvas.py
@@ -128,10 +128,81 @@ class Scene(QGraphicsScene):
     def scene_bottom(self):
         return 20000.0
 
+class ForegroundMarker(object):
+    def boundingRect(self, view):
+        '''
+        Return the bounding rectangle of the marker in view coordinates.
+        '''
+        raise NotImplementedError
+
+    def paint(self, view, painter, rect):
+        '''
+        Paint the marker for the given view using the painter and rect, which are both in scene coordinates.
+        '''
+        pass
+
+class HorizontalSnapMarker(ForegroundMarker):
+    def __init__(self, y):
+        self.y = y
+
+    def bounding_rect(self, view):
+        pos_y = view.viewportTransform().map(QPointF(0.0, float(self.y))).y()
+        return QRectF(0.0, pos_y - (view_snap_marker_width / 2.0), view.viewport().width(), view.snap_marker_width)
+
+    def paint(self, view, painter, rect):
+        pos_y = painter.transform().map(QPointF(0.0, float(self.y))).y()
+        rect = painter.transform().mapRect(rect)
+
+        painter.save()
+        painter.resetTransform()
+
+        gradient = QLinearGradient(0.0, pos_y, 0.0, pos_y + view.snap_marker_width / 2.0)
+        gradient.setSpread(QGradient.ReflectSpread)
+        gradient.setStops([
+            (0.0, QColor.fromRgbF(1.0, 1.0, 1.0, 1.0)),
+            (0.5, QColor.fromRgbF(view.snap_marker_color.redF(), view.snap_marker_color.greenF(), view.snap_marker_color.blueF(), 0.5)),
+            (1.0, QColor.fromRgbF(0.0, 0.0, 0.0, 0.0))])
+
+        painter.setPen(Qt.transparent)
+        painter.setBrush(QBrush(gradient))
+        painter.drawRect(QRectF(rect.x(), pos_y - (view.snap_marker_width / 2.0), rect.width(), view.snap_marker_width))
+
+        painter.restore()
+
+class VerticalSnapMarker(ForegroundMarker):
+    def __init__(self, frame):
+        self.frame = frame
+
+    def bounding_rect(self, view):
+        pos_x = view.viewportTransform().map(QPointF(float(self.frame), 0.0)).x()
+        return QRectF(pos_x - (view.snap_marker_width / 2.0), 0.0, view.snap_marker_width, view.viewport().height())
+
+    def paint(self, view, painter, rect):
+        pos_x = painter.transform().map(QPointF(float(self.frame), 0.0)).x()
+        rect = painter.transform().mapRect(rect)
+
+        painter.save()
+        painter.resetTransform()
+
+        gradient = QLinearGradient(pos_x, 0.0, pos_x + view.snap_marker_width / 2.0, 0.0)
+        gradient.setSpread(QGradient.ReflectSpread)
+        gradient.setStops([
+            (0.0, QColor.fromRgbF(1.0, 1.0, 1.0, 1.0)),
+            (0.5, QColor.fromRgbF(view.snap_marker_color.redF(), view.snap_marker_color.greenF(), view.snap_marker_color.blueF(), 0.5)),
+            (1.0, QColor.fromRgbF(0.0, 0.0, 0.0, 0.0))])
+
+        painter.setPen(Qt.transparent)
+        painter.setBrush(QBrush(gradient))
+        painter.drawRect(QRectF(pos_x - (view.snap_marker_width / 2.0), rect.y(), view.snap_marker_width, rect.height()))
+
+        painter.restore()
+
 class View(QGraphicsView):
     black_pen = QPen(QColor.fromRgbF(0.0, 0.0, 0.0))
     white_pen = QPen(QColor.fromRgbF(1.0, 1.0, 1.0))
     handle_width = 10.0
+    snap_marker_color = QColor.fromRgbF(0.0, 1.0, 0.0)
+    snap_marker_width = 5.0
 
     def __init__(self, clock, space, source_list):
         QGraphicsView.__init__(self)
