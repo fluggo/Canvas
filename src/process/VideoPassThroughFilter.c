422baff57cfb8857b5612f4604b76ed01f9369ad
Brian Crowell <brian@fluggo.com>
2011-06-05 23:16:52 -0500

    Add start_frame and end_frame to VideoPassThroughFilter
    This lets someone limit what the pass through filter will bother producing.

--- src/process/VideoPassThroughFilter.c
+++ src/process/VideoPassThroughFilter.c
@@ -26,17 +26,38 @@ typedef struct {
     PyObject_HEAD
 
     video_source *source;
-    int offset;
+    int offset, start_frame, end_frame;
+    bool start_frame_valid, end_frame_valid;
     GStaticRWLock rwlock;
 } py_obj_VideoPassThroughFilter;
+
+static int VideoPassThroughFilter_set_start_frame( py_obj_VideoPassThroughFilter *self, PyObject *value, void *closure );
+static int VideoPassThroughFilter_set_end_frame( py_obj_VideoPassThroughFilter *self, PyObject *value, void *closure );
 
 static int
 VideoPassThroughFilter_init( py_obj_VideoPassThroughFilter *self, PyObject *args, PyObject *kwds ) {
     PyObject *source;
     self->offset = 0;
+    self->start_frame_valid = false;
+    self->end_frame_valid = false;
 
-    if( !PyArg_ParseTuple( args, "O|i", &source, &self->offset ) )
+    PyObject *start_frame_obj = NULL, *end_frame_obj = NULL;
+
+    static char *kwlist[] = { "source", "offset", "start_frame", "end_frame", NULL };
+
+    if( !PyArg_ParseTupleAndKeywords( args, kwds, "O|iOO", kwlist,
+            &source, &self->offset, &start_frame_obj, &end_frame_obj ) )
         return -1;
+
+    if( start_frame_obj != NULL ) {
+        if( VideoPassThroughFilter_set_start_frame( self, start_frame_obj, NULL ) == -1 )
+            return -1;
+    }
+
+    if( end_frame_obj != NULL ) {
+        if( VideoPassThroughFilter_set_end_frame( self, end_frame_obj, NULL ) == -1 )
+            return -1;
+    }
 
     if( !py_video_take_source( source, &self->source ) )
         return -1;
@@ -49,21 +70,51 @@ VideoPassThroughFilter_init( py_obj_VideoPassThroughFilter *self, PyObject *args
 static void
 VideoPassThroughFilter_getFrame( py_obj_VideoPassThroughFilter *self, int frameIndex, rgba_frame_f16 *frame ) {
     g_static_rw_lock_reader_lock( &self->rwlock );
-    video_get_frame_f16( self->source, frameIndex + self->offset, frame );
+
+    if( self->start_frame_valid && frameIndex < self->start_frame ) {
+        box2i_set_empty( &frame->current_window );
+    }
+    else if( self->end_frame_valid && frameIndex >= self->end_frame ) {
+        box2i_set_empty( &frame->current_window );
+    }
+    else {
+        video_get_frame_f16( self->source, frameIndex + self->offset, frame );
+    }
+
     g_static_rw_lock_reader_unlock( &self->rwlock );
 }
 
 static void
 VideoPassThroughFilter_getFrame32( py_obj_VideoPassThroughFilter *self, int frameIndex, rgba_frame_f32 *frame ) {
     g_static_rw_lock_reader_lock( &self->rwlock );
-    video_get_frame_f32( self->source, frameIndex + self->offset, frame );
+
+    if( self->start_frame_valid && frameIndex < self->start_frame ) {
+        box2i_set_empty( &frame->current_window );
+    }
+    else if( self->end_frame_valid && frameIndex >= self->end_frame ) {
+        box2i_set_empty( &frame->current_window );
+    }
+    else {
+        video_get_frame_f32( self->source, frameIndex + self->offset, frame );
+    }
+
     g_static_rw_lock_reader_unlock( &self->rwlock );
 }
 
 static void
 VideoPassThroughFilter_getFrameGL( py_obj_VideoPassThroughFilter *self, int frameIndex, rgba_frame_gl *frame ) {
     g_static_rw_lock_reader_lock( &self->rwlock );
-    video_get_frame_gl( self->source, frameIndex + self->offset, frame );
+
+    if( self->start_frame_valid && frameIndex < self->start_frame ) {
+        video_get_frame_gl( NULL, frameIndex + self->offset, frame );
+    }
+    else if( self->end_frame_valid && frameIndex >= self->end_frame ) {
+        video_get_frame_gl( NULL, frameIndex + self->offset, frame );
+    }
+    else {
+        video_get_frame_gl( self->source, frameIndex + self->offset, frame );
+    }
+
     g_static_rw_lock_reader_unlock( &self->rwlock );
 }
 
@@ -140,6 +191,56 @@ VideoPassThroughFilter_set_offset( py_obj_VideoPassThroughFilter *self, PyObject
     return 0;
 }
 
+static PyObject *
+VideoPassThroughFilter_get_start_frame( py_obj_VideoPassThroughFilter *self, void *closure ) {
+    if( self->start_frame_valid )
+        return PyInt_FromLong( self->start_frame );
+
+    Py_RETURN_NONE;
+}
+
+static int
+VideoPassThroughFilter_set_start_frame( py_obj_VideoPassThroughFilter *self, PyObject *value, void *closure ) {
+    if( value == Py_None ) {
+        self->start_frame_valid = false;
+        return 0;
+    }
+
+    int start_frame = PyInt_AsLong( value );
+
+    if( start_frame == -1 && PyErr_Occurred() )
+        return -1;
+
+    self->start_frame = start_frame;
+    self->start_frame_valid = true;
+    return 0;
+}
+
+static PyObject *
+VideoPassThroughFilter_get_end_frame( py_obj_VideoPassThroughFilter *self, void *closure ) {
+    if( self->end_frame_valid )
+        return PyInt_FromLong( self->end_frame );
+
+    Py_RETURN_NONE;
+}
+
+static int
+VideoPassThroughFilter_set_end_frame( py_obj_VideoPassThroughFilter *self, PyObject *value, void *closure ) {
+    if( value == Py_None ) {
+        self->end_frame_valid = false;
+        return 0;
+    }
+
+    int end_frame = PyInt_AsLong( value );
+
+    if( end_frame == -1 && PyErr_Occurred() )
+        return -1;
+
+    self->end_frame = end_frame;
+    self->end_frame_valid = true;
+    return 0;
+}
+
 static video_frame_source_funcs sourceFuncs = {
     .get_frame = (video_get_frame_func) VideoPassThroughFilter_getFrame,
     .get_frame_32 = (video_get_frame_32_func) VideoPassThroughFilter_getFrame32,
@@ -155,6 +256,8 @@ VideoPassThroughFilter_getFuncs( py_obj_VideoPassThroughFilter *self, void *clos
 static PyGetSetDef VideoPassThroughFilter_getsetters[] = {
     { VIDEO_FRAME_SOURCE_FUNCS, (getter) VideoPassThroughFilter_getFuncs, NULL, "Video frame source C API." },
     { "offset", (getter) VideoPassThroughFilter_get_offset, (setter) VideoPassThroughFilter_set_offset, "Get or set the offset." },
+    { "start_frame", (getter) VideoPassThroughFilter_get_start_frame, (setter) VideoPassThroughFilter_set_start_frame, "Get or set the start frame, inclusive." },
+    { "end_frame", (getter) VideoPassThroughFilter_get_end_frame, (setter) VideoPassThroughFilter_set_end_frame, "Get or set the end frame, exclusive." },
     { NULL }
 };
 
