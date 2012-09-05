8388a5b2158f5d24cee266c3e1d5ad29a9878997
Brian Crowell <brian@fluggo.com>
2012-09-05 16:54:42 -0500

    cprocess: Update video frame getters to use GL method if available
    Up until now, we couldn't be sure we would have a GL context available to
    us. Now, these functions ask for the thread-local GL context allocated in
    gl.c if one is not already active. This is another step toward going
    GL-only.

--- src/cprocess/main.c
+++ src/cprocess/main.c
@@ -30,92 +30,114 @@ get_time_frame( const rational *frameRate, int64_t time ) {
     return (time * (int64_t)(frameRate->n)) / (INT64_C(1000000000) * (int64_t)(frameRate->d));
 }
 
-EXPORT void video_get_frame_f16( video_source *source, int frameIndex, rgba_frame_f16 *targetFrame ) {
+EXPORT void video_get_frame_f16( video_source *source, int frame_index, rgba_frame_f16 *frame ) {
     if( !source || !source->funcs ) {
-        box2i_set_empty( &targetFrame->current_window );
+        box2i_set_empty( &frame->current_window );
         return;
     }
 
     if( source->funcs->get_frame ) {
-        source->funcs->get_frame( source->obj, frameIndex, targetFrame );
-        return;
+        source->funcs->get_frame( source->obj, frame_index, frame );
     }
+    else if( source->funcs->get_frame_32 ) {
+        // Allocate a new frame
+        rgba_frame_f32 temp_frame;
+        v2i size;
 
-    if( !source->funcs->get_frame_32 ) {
-        box2i_set_empty( &targetFrame->current_window );
-        return;
-    }
+        box2i_get_size( &frame->full_window, &size );
+        temp_frame.data = g_slice_alloc( sizeof(rgba_f32) * size.x * size.y );
+        temp_frame.full_window = frame->full_window;
+        temp_frame.current_window = frame->full_window;
 
-    // Allocate a new frame
-    rgba_frame_f32 tempFrame;
-    v2i size;
+        source->funcs->get_frame_32( source->obj, frame_index, &temp_frame );
 
-    box2i_get_size( &targetFrame->full_window, &size );
-    tempFrame.data = g_slice_alloc( sizeof(rgba_f32) * size.x * size.y );
-    tempFrame.full_window = targetFrame->full_window;
-    tempFrame.current_window = targetFrame->full_window;
+        if( !box2i_is_empty( &temp_frame.current_window ) ) {
+            // Convert to f16
+            int countX = temp_frame.current_window.max.x - temp_frame.current_window.min.x + 1;
 
-    source->funcs->get_frame_32( source->obj, frameIndex, &tempFrame );
-
-    if( !box2i_is_empty( &tempFrame.current_window ) ) {
-        // Convert to f16
-        int countX = tempFrame.current_window.max.x - tempFrame.current_window.min.x + 1;
-
-        if( countX > 0 ) {
-            for( int y = tempFrame.current_window.min.y; y <= tempFrame.current_window.max.y; y++ ) {
-                rgba_f32_to_f16(
-                    video_get_pixel_f16( targetFrame, tempFrame.current_window.min.x, y ),
-                    video_get_pixel_f32( &tempFrame, tempFrame.current_window.min.x, y ),
-                    countX );
+            if( countX > 0 ) {
+                for( int y = temp_frame.current_window.min.y; y <= temp_frame.current_window.max.y; y++ ) {
+                    rgba_f32_to_f16(
+                        video_get_pixel_f16( frame, temp_frame.current_window.min.x, y ),
+                        video_get_pixel_f32( &temp_frame, temp_frame.current_window.min.x, y ),
+                        countX );
+                }
             }
         }
+
+        frame->current_window = temp_frame.current_window;
+
+        g_slice_free1( sizeof(rgba_f32) * size.x * size.y, temp_frame.data );
     }
+    else if( source->funcs->get_frame_gl ) {
+        // Load a GL texture, then transfer it to memory
+        gl_ensure_context();
 
-    targetFrame->current_window = tempFrame.current_window;
+        rgba_frame_gl temp_frame = { .full_window = frame->full_window };
+        source->funcs->get_frame_gl( source->obj, frame_index, &temp_frame );
 
-    g_slice_free1( sizeof(rgba_f32) * size.x * size.y, tempFrame.data );
+        // Default pixel store values should work just fine
+        glGetTexImage( GL_TEXTURE_RECTANGLE, 0, GL_RGBA, GL_HALF_FLOAT_ARB, frame->data );
+        glDeleteTextures( 1, &temp_frame.texture );
+
+        frame->current_window = temp_frame.current_window;
+    }
+    else {
+        box2i_set_empty( &frame->current_window );
+    }
 }
 
-EXPORT void video_get_frame_f32( video_source *source, int frameIndex, rgba_frame_f32 *targetFrame ) {
+EXPORT void video_get_frame_f32( video_source *source, int frame_index, rgba_frame_f32 *frame ) {
     if( !source || !source->funcs ) {
-        box2i_set_empty( &targetFrame->current_window );
+        box2i_set_empty( &frame->current_window );
         return;
     }
 
     if( source->funcs->get_frame_32 ) {
-        source->funcs->get_frame_32( source->obj, frameIndex, targetFrame );
-        return;
+        source->funcs->get_frame_32( source->obj, frame_index, frame );
     }
+    else if( source->funcs->get_frame ) {
+        // Allocate a new frame
+        rgba_frame_f16 temp_frame;
+        v2i size;
 
-    if( !source->funcs->get_frame ) {
-        box2i_set_empty( &targetFrame->current_window );
-        return;
+        box2i_get_size( &frame->full_window, &size );
+        temp_frame.data = g_slice_alloc( sizeof(rgba_f16) * size.x * size.y );
+        temp_frame.full_window = frame->full_window;
+        temp_frame.current_window = frame->full_window;
+
+        source->funcs->get_frame( source->obj, frame_index, &temp_frame );
+
+        // Convert to f32
+        int countX = temp_frame.current_window.max.x - temp_frame.current_window.min.x + 1;
+
+        for( int y = temp_frame.current_window.min.y; y <= temp_frame.current_window.max.y; y++ ) {
+            rgba_f16_to_f32(
+                video_get_pixel_f32( frame, temp_frame.current_window.min.x, y ),
+                video_get_pixel_f16( &temp_frame, temp_frame.current_window.min.x, y ),
+                countX );
+        }
+
+        frame->current_window = temp_frame.current_window;
+
+        g_slice_free1( sizeof(rgba_f16) * size.x * size.y, temp_frame.data );
     }
+    else if( source->funcs->get_frame_gl ) {
+        // Load a GL texture, then transfer it to memory
+        gl_ensure_context();
 
-    // Allocate a new frame
-    rgba_frame_f16 tempFrame;
-    v2i size;
+        rgba_frame_gl temp_frame = { .full_window = frame->full_window };
+        source->funcs->get_frame_gl( source->obj, frame_index, &temp_frame );
 
-    box2i_get_size( &targetFrame->full_window, &size );
-    tempFrame.data = g_slice_alloc( sizeof(rgba_f16) * size.x * size.y );
-    tempFrame.full_window = targetFrame->full_window;
-    tempFrame.current_window = targetFrame->full_window;
+        // Default pixel store values should work just fine
+        glGetTexImage( GL_TEXTURE_RECTANGLE, 0, GL_RGBA, GL_FLOAT, frame->data );
+        glDeleteTextures( 1, &temp_frame.texture );
 
-    source->funcs->get_frame( source->obj, frameIndex, &tempFrame );
-
-    // Convert to f32
-    int countX = tempFrame.current_window.max.x - tempFrame.current_window.min.x + 1;
-
-    for( int y = tempFrame.current_window.min.y; y <= tempFrame.current_window.max.y; y++ ) {
-        rgba_f16_to_f32(
-            video_get_pixel_f32( targetFrame, tempFrame.current_window.min.x, y ),
-            video_get_pixel_f16( &tempFrame, tempFrame.current_window.min.x, y ),
-            countX );
+        frame->current_window = temp_frame.current_window;
     }
-
-    targetFrame->current_window = tempFrame.current_window;
-
-    g_slice_free1( sizeof(rgba_f16) * size.x * size.y, tempFrame.data );
+    else {
+        box2i_set_empty( &frame->current_window );
+    }
 }
 
 
