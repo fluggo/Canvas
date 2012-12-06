67d12ce289d3cfdc2e6b1589ba8ac2737382b02b
Brian Crowell <brian@fluggo.com>
2012-12-06 08:00:53 -0600

    Extended arguments for the test runner

--- testrunner.py
+++ testrunner.py
@@ -4,8 +4,67 @@ import sys
 import os.path
 import unittest
 
-path = sys.argv[1]
-names = sys.argv[2:]
+# Grab command-line arguments
+import argparse
+
+argparser = argparse.ArgumentParser()
+argparser.add_argument('path', help='Path to the test script.')
+argparser.add_argument('testnames', metavar='testname', nargs='*',
+    help='Optional name of a test to run. This can be a class name or class and method.')
+argparser.add_argument('--log-level', dest='log_level',
+                       choices=['debug', 'info', 'warning', 'error', 'critical'],
+                       default='warning',
+                       help='Logging level to use for the root logger.')
+argparser.add_argument('--log', dest='log_filter', action='append',
+                       help='Log debug events from the given module.',
+                       default=[])
+argparser.add_argument('--log-glib', dest='enable_glib_logging', action='store_true',
+                       help='Enable event logging from GLib and C processing libraries.', default=False)
+
+args = argparser.parse_args()
+
+# Set up logging
+if args.enable_glib_logging:
+    import fluggo.media.process
+    fluggo.media.process.enable_glib_logging(args.enable_glib_logging)
+
+if True:
+    import fluggo.logging
+    import logging
+    handler = logging.StreamHandler()
+    handler.setLevel(logging.NOTSET)
+    handler.setFormatter(logging.Formatter('{levelname}:{name}:{msg}', style='{'))
+
+    root_logger = logging.getLogger()
+    root_logger.setLevel(logging.NOTSET)
+    root_logger.addHandler(handler)
+
+    rootno = getattr(logging, args.log_level.upper())
+
+    filtereq = [arg for arg in args.log_filter]
+    filterstart = [arg + '.' for arg in args.log_filter]
+
+    class MyFilter:
+        def filter(self, record):
+            for name in filtereq:
+                if name == record.name:
+                    return True
+
+            for name in filterstart:
+                if record.name.startswith(name):
+                    return True
+
+            if record.levelno >= rootno:
+                return True
+
+            return False
+
+    handler.addFilter(MyFilter())
+
+
+
+path = args.path
+names = args.testnames
 
 sys.path.insert(0, os.path.dirname(path))
 
